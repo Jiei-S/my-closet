0ca8a3367c23b766493c929ec9177513b0dc2685
Jiei-S <jiei7414@gmail.com>
2021-02-19 21:51:27 +0900

    navigation base

--- frontend/js/footer.tsx
+++ frontend/js/footer.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 
 const Footer: React.FC = (): React.ReactElement => (
-	<footer className='footer'>
-		<div className='footer__inner'>
-			<small>&copy; 2021 JIEI</small>
-		</div>
-	</footer>
+  <footer className='footer'>
+    <div className='footer__inner'>
+      <small>&copy; 2021 JIEI</small>
+    </div>
+  </footer>
 );
 
 
